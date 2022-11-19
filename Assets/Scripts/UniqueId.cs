1b6e21908f1eee025122a05b06ef620789fb41d0
Tetrergeru <ArutunoffOleg@yandex.ru>
2022-11-19 16:03:11 +0400

    Add URP, Add UniqueId script and prefab

---
+++ Assets/Scripts/UniqueId.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class UniqueId : MonoBehaviour
+{
+    public string Id;
+
+    void Start()
+    {
+        if (Id == null || Id == "")
+            Id = RandomId(10);
+    }
+
+    string RandomId(int length)
+    {
+        const string symbols = "0123456789abcdef";
+        var res = new StringBuilder();
+        for (var i = 0; i < length; i++)
+            res.Append(symbols[Random.Range(0, symbols.Length)]);
+        return res.ToString();
+    }
+}
